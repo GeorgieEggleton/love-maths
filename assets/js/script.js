e525092687c4193e95ee29a0a506230838dbd596
GeorgieEggleton <georgie@sailingfaster.co.uk>
2023-03-14 09:01:45 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,40 @@
-function runGame(){
+//wait for the DOM to finish loading before running this game
+// Get the button elements and add event listners
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    runGame("addition");
+
+});
+
+/**
+ * the main game loop, called when the script is first looped
+ * and after the users answer has been processed. 
+ */
+function runGame(gameType){
+
+    // creates two random numbers. 
+    let num1 = Math.floor(Math.random() * 25)+1;
+    let num2 = Math.floor(Math.random() * 25)+1;
+
+    if (gameType === "addition") {
+        dispalyAdditionQuestion(num1, num2);
+    } else { alert (`Unknown game type: ${gameType}`);
+      throw `Unknown game type: ${gameType}. Aborting! `;
+
+    }
 
 }
 
@@ -18,14 +54,16 @@ function incrementWrongAnswer(){
 
 }
 
-dispalyAdditionQuestion(){
+function dispalyAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+}
+
+function dispalySubtractQuestion(){
 
 }
 
-dispalySubtractQuestion(){
+function displayMultiplyQuestion(){
 
-}
-
-displayMultiplyQuestion(){
-    
 }
